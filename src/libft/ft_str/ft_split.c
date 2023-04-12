1195fb2e42813f0ca15fb234106fb0920edf403b
Lin <lsun@student.hive.fi>
2023-04-12 16:23:57 +0300

    refactor: ft_split

--- src/libft/ft_str/ft_split.c
+++ src/libft/ft_str/ft_split.c
@@ -3,110 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lliu <lliu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:23:32 by lliu              #+#    #+#             */
-/*   Updated: 2022/11/12 21:29:10 by lliu             ###   ########.fr       */
+/*   Updated: 2023/04/12 16:22:07 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*find_word(char const *s, char c)
+static size_t	ft_word_count(const char *s, char c)
 {
-	char		*word;
-	char		*temp;
-	char const	*temp_word;
-	int			len_word;
-
-	len_word = 0;
-	temp_word = s;
-	while (*temp_word != c && *temp_word)
-	{
-		len_word++;
-		temp_word++;
-	}
-	temp = (char *)malloc(len_word + 1);
-	if (!temp)
-		return (NULL);
-	word = temp;
-	while (*s != c && *s)
-		*temp++ = *s++;
-	*temp = 0;
-	return (word);
-}
-
-static int	ft_count_words(const char *s, char c)
-{
-	int	num_c;
-
-	num_c = 0;
-	while (*s)
-	{
-		while (*s == c && *s)
-			s++;
-		if (*s != c && *s)
-			num_c++;
-		while (*s != c && *s)
-			s++;
-	}
-	return (num_c);
-}
-
-static char	*ft_fornorm(char const *temp, char c)
-{
-	if (!temp)
-		return (NULL);
-	while (*temp == c && *temp)
-		temp++;
-	return ((char *)temp);
-}
-
-void	ft_split_fornorm(char **dst, char *sub_s, char c)
-{
-	int	i;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
-	while (*sub_s)
+	count = 0;
+	while (s[i])
 	{
-		sub_s = ft_fornorm(sub_s, c);
-		dst[i] = find_word(sub_s, c);
-		if (!dst[i])
+		if (s[i] != c)
 		{
-			while (i >= 0)
+			count++;
+			while (s[i] != c && s[i])
 			{
-				free(dst[i]);
-				i--;
+				i++;
 			}
-			free(dst);
-			return ;
 		}
-		i++;
-		sub_s = ft_strchr(sub_s, c);
-		sub_s = ft_fornorm(sub_s, c);
-		if (!sub_s)
-			break ;
+		else
+			i++;
 	}
-	dst[i] = 0;
-	return ;
+	return (count);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	*sub_s;
-	char	**dst;
+	char	**str_arr;
+	size_t	i;
+	size_t	len;
 
 	if (!s)
 		return (NULL);
-	dst = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
-	if (!dst)
+	str_arr = (char **)malloc(sizeof(char *) * (ft_word_count(s, c) + 1));
+	if (!str_arr)
 		return (NULL);
-	if (ft_count_words(s, c) != 0)
+	i = 0;
+	while (*s)
 	{
-		sub_s = (char *)s;
-		ft_split_fornorm(dst, sub_s, c);
+		if (*s != c)
+		{
+			len = 0;
+			while (*s != c && *s && ++len)
+				s++;
+			str_arr[i++] = ft_substr(s - len, 0, len);
+		}
+		else
+			s++;
 	}
-	else
-		dst[0] = 0;
-	return (dst);
+	str_arr[i] = 0;
+	return (str_arr);
 }
